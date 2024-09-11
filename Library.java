93a7b654158bb0af49226c2e4845dea1f9a35c49
AnkurBarik-BCA <bwubca23055@brainwareuniversity.ac.in>
2024-09-11 12:04:28 +0530

    .

--- Library.java
+++ Library.java
@@ -19,63 +19,47 @@ class Member {
     Member() {
         name = "";
         id = "";
-        borrowedbooks = new String[0];
+        borrowedbooks = new Book[3];
+        bookcount = 0;
     }
 
     Member(String n, String i, String[] b) {
         name = n;
         id = i;
-        borrowedbooks = b;
-    }
-
-    class Book {
-        String title, author;
-        long isbn;
-        boolean avl;
-
-        Book(String t, String aut, long i, boolean a) {
-            title = t;
-            author = aut;
-            isbn = i;
-            avl = a;
+        borrowedbooks = new Book[3];
+        for (int j = 0; j < b.length && j < 3; j++) {
+            borrowedbooks[j] = new Book(b[j], "", 0, false);
+            bookcount++;
         }
     }
 
-    class Member {
-        String name, id;
-        Book[] borrowedbooks;
-
-        Member() {
-            name = "";
-            id = "";
-            borrowedbooks = new String[0];
+    void borrowedBooks(Book book) {
+        if (bookcount < 3 && book.avl) {
+            borrowedbooks[bookcount] = book;
+            bookcount++;
+        } else {
+            System.out.println("Sorry, you can't borrow more than 3 books or the book is not available");
         }
+    }
 
-        Member(String n, String i, String[] b) {
-            name = n;
-            id = i;
-            borrowedbooks = b;
-        }
-
-        void borrowedBooks() {
-            if (bookcount < 3 && Book.avl) {
-                borrowedbooks[bookcount] = Book;
-                bookcount++;
-            } else {
-                System.out.println("Sorry, you can't borrow more than 3 books or the book is not available");
+    void returnedBook(Book book) {
+        for (int i = 0; i < borrowedbooks.length; i++) {
+            if (borrowedbooks[i] == book) {
+                borrowedbooks[i] = null;
+                book.avl = true;
+                bookcount--;
+                System.out.println("\n\n" + name + " returned " + book.title + "\n\n");
+                break;
             }
         }
-
     }
 
-    public class Library {
-        public static void main(String[] args) {
-            Member m = new Member();
-            Member m1 = new Member("Bikram", "BWU/BCA/23/036",
-                    new String[] { "The Count of Monte Cristo", "Feluda Samagra",
-                            "Prof. Shanku Samagra", "Three Musketeers", "The Man With Iron Mask" });
-            // System.out.print(m1.borrowedbooks[1]);
-            m1.borrowedBooks();
+    void showBorrowedBooks() {
+        System.out.println(name + " has borrowed:");
+        for (Book book : borrowedbooks) {
+            if (book != null) {
+                System.out.println("- " + book.title);
+            }
         }
     }
 }
@@ -85,9 +69,18 @@ public class Library {
         Member m = new Member();
         Member m1 = new Member("Bikram", "BWU/BCA/23/036", new String[] { "The Count of Monte Cristo", "Feluda Samagra",
                 "Prof. Shanku Samagra", "Three Musketeers", "The Man With Iron Mask" });
-        // System.out.print(m1.borrowedbooks[1]);
-        for (String book : m1.borrowedbooks) {
-            System.out.println(book);
-        }
+        Member m2 = new Member("John", "BWU/BCA/23/037",
+                new String[] { "The Lord of the Rings", "Harry Potter", "The Hobbit" });
+        Member m3 = new Member("Alice", "BWU/BCA/23/038",
+                new String[] { "Pride and Prejudice", "Jane Eyre", "Little Women" });
+
+        Book returnedBook = m1.borrowedbooks[0];
+        m1.returnedBook(returnedBook);
+
+        m1.showBorrowedBooks();
+        System.out.println();
+        m2.showBorrowedBooks();
+        System.out.println();
+        m3.showBorrowedBooks();
     }
 }
